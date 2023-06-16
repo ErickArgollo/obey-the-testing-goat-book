4278dec4c9de612d21d085aa1f21c4c2e8ad09eb
Erick Argollo <argolloerickdev@gmail.com>
2023-06-16 18:15:58 -0300

    feat: https://www.obeythetestinggoat.com/book/chapter_philosophy_and_refactoring.html\#_using_selenium_to_test_user_interactions

--- functional_tests.py
+++ functional_tests.py
@@ -4,12 +4,18 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox(timeout=10)
+
     def tearDown(self) -> None:
         self.browser.quit()
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
+
         self.assertIn('To-Do',self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
+        self.fail('Finish the test!')
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
